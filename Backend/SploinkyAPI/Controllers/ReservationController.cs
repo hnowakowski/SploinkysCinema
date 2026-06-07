9c80d5ffa0e9a58c99ba2bf12a3409580ac245db
hnowakowski <h.nowakowski.praca@gmail.com>
2026-06-07 14:14:10 +0200

    All CRUD operations for reservations

--- Backend/SploinkyAPI/Controllers/ReservationController.cs
+++ Backend/SploinkyAPI/Controllers/ReservationController.cs
@@ -31,18 +31,37 @@ namespace SploinkyAPI.Controllers
 
         [HttpGet]
         [Route("api/reservations/get")]
-        public async Task<List<Reservation>> Get(int Id)
+        public async Task<Reservation> Get(Guid Id)
         {
-            throw new NotImplementedException();
+            IMapper mapper = new Mapper(_session);
+            // fetch returns an ienumerable so i have to grab it like that even if i only care about one item
+            IEnumerable<Reservation> reservations = await mapper.FetchAsync<Reservation>("SELECT * FROM RESERVATION WHERE id = ?;", Id);
+            return reservations.First(); //guids *should* be unique so i assume this will always have one element
         }
 
         [HttpPost]
-        [Route("api/reservations/put")]
-        public async Task<HttpStatusCode> Put(Reservation reservation)
+        [Route("api/reservations/insert")]
+        public async Task Insert(Reservation reservation)
         {
-            throw new NotImplementedException();
+            IMapper mapper = new Mapper(_session);
+            await mapper.InsertAsync<Reservation>(reservation);
         }
 
+        [HttpPost]
+        [Route("api/reservations/update")]
+        public async Task Update(Reservation reservation)
+        {
+            IMapper mapper = new Mapper(_session);
+            await mapper.UpdateAsync<Reservation>(reservation);
+        }
 
+        [HttpPost]
+        [Route("api/reservations/delete")]
+        // the mapper takes entire object as input but deletes ONLY based on the id, other attribs dont have to match
+        public async Task Delete(Reservation reservation)
+        {
+            IMapper mapper = new Mapper(_session);
+            await mapper.DeleteAsync<Reservation>(reservation);
+        }
     }
 }
